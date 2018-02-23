1fb1f672adab1aaddacb079b44556f26b0edf295
veysiertekin <ertekin.veysi@gmail.com>
2018-02-23 17:29:43 +0300

    fixes invalid no-request-journal flag while recording traffic with wiremock

--- wiremock-demo/wiremock-standalone-base/start_wiremock.sh
+++ wiremock-demo/wiremock-standalone-base/start_wiremock.sh
@@ -13,8 +13,7 @@ exec java -XX:+UseG1GC \
     -jar wiremock-standalone-2.15.0.jar \
     --port=${HTTP_PORT:-80} \
     --https-port=${HTTPS_PORT:-443} \
-    `[ "$RECORD_MAPPINGS" = "true" ] && echo "--record-mappings" || echo ""` \
-    --no-request-journal \
+    `[ "$RECORD_MAPPINGS" = "true" ] && echo "--record-mappings" || echo "--no-request-journal"` \
     `[ "$VERBOSE" = "true" ] && echo "--verbose" || echo ""` \
     --global-response-templating \
     --preserve-host-header
